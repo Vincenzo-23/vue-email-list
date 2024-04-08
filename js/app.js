9cbe53ed0b5b8c5ee60b0b5b6302b48bbf42c12b
Vincenzo-23 <specialevincenzo88@gmail.com>
2024-04-08 15:06:02 +0200

    ADD: added the button that generate the list of emails with the js function

--- js/app.js
+++ js/app.js
@@ -3,7 +3,24 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        message: 'Hello Vue!'
+        message: 'Lista Email',
+        emails: [],
       }
+    },
+    methods: {
+        fetchEmails(n){
+            for(let i = 0; i < n; i++){
+
+                axios
+                    .get(`https://flynn.boolean.careers/exercises/api/random/mail`)
+                    .then((res) => {
+                        this.emails.push(res.data.response)
+                    })
+
+            }
+        },
+    },
+    created() {
+        // this.fetchEmails(10)
     }
   }).mount('#app')
